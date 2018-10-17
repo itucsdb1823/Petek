ce84e5c98af92ae0d46ad955b51c51d1446da757
Yavuz KOCA <ykoca0808@gmail.com>
2018-10-17 20:29:59 +0300

    Authenticate users in server

--- server/models/User.py
+++ server/models/User.py
@@ -1,19 +1,58 @@
 from server.models.Base import Base
-from server import cur, conn, server, jwt
+from server import conn, server, jwt, bcrypt
+import datetime
+import string
+import random
+import time
+from flask_jwt_simple import create_jwt, jwt_required, get_jwt_identity
+import psycopg2.extras
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 class User(Base):
     name = ''
     password = ''
+    email = ''
+    token = ''
 
-    def __init__(self, name, password):
+    def __init__(self, name, password, email):
         self.name = name
         self.password = password
+        self.email = email
 
     @staticmethod
-    def get(name, password):
-        cur.execute("SELECT * FROM users WHERE name=%s", (name,))
+    def get(email, password):
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("SELECT * FROM users WHERE email=%s LIMIT 1", (email,))
         user = cur.fetchone()
-        if user and user['password'] == password:
+        cur.close()
+        if user and bcrypt.check_password_hash(user['password'], password):
             return user
         return None
+
+    def create(self):
+        hashed_password = bcrypt.generate_password_hash(self.password).decode('utf-8')
+        ts = time.time()
+        created_at = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+        # check uniqueness of the user, create slug from name and check its uniqueness
+
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("INSERT INTO users(name, email, password, slug, created_at) VALUES(%s, %s, %s, %s, %s) returning id",
+                    (self.name, self.email, hashed_password, id_generator(20), str(created_at)))
+
+        user_id = cur.fetchone()['id']
+
+        token = {'jwt': create_jwt(identity={
+            'id': user_id
+
+        })}
+
+        cur.execute("INSERT INTO tokens(user_id, token, created_at) VALUES(%s, %s, %s)", (str(user_id), str(token['jwt']), str(created_at)))
+
+        conn.commit()
+        cur.close()
+        return token
--- server/resources/auth.py
+++ server/resources/auth.py
@@ -2,47 +2,47 @@ from flask_restful import Resource, reqparse
 from flask import jsonify
 from flask_jwt_simple import create_jwt, jwt_required, get_jwt_identity
 from server.models.User import User
+from server import cur, conn
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', type=str, help='Name must be a string')
 parser.add_argument('password', type=str, help='Password must be a string')
+parser.add_argument('password_confirm', type=str, help='Password must be a string')
+parser.add_argument('email', type=str, help='Email must be a string')
 
-<<<<<<< HEAD
-class Auth(Resource):
-    def get(self):
-        user = User(username='yavuz', password='asdf')
-        user.save()
-        command = (
-            """
-            CREATE TABLE users (
-                name VARCHAR(255),
-                email VARCHAR(255) UNIQUE
-            )
-            """)
-        cur.execute(command)
-        conn.commit()
-=======
->>>>>>> 2e7f5a96e9caaa9bdc96c5365c3d597c2ce57ab6
 
 class Register(Resource):
-    def get(self):
+    def post(self):
+        args = parser.parse_args()
+        name = args['name']
+        password = args['password']
+        password_confirm = args['password_confirm']
+        email = args['email']
+
+        # Validation rules will be applied here
+
+        user = User(name, password, email)
+        token = user.create()
+
         return jsonify({
-            'sa': 'as'
+            'access_token': token,
+            'data': {
+                'name': name,
+                'email': email
+            }
         })
 
 
 class Login(Resource):
     def post(self):
         args = parser.parse_args()
-        name = args['name']
+        email = args['email']
         password = args['password']
 
-        user = User.get(name, password)
+        user = User.get(email, password)
 
         if user is not None:
             token = {'jwt': create_jwt(identity={
-                'name': user['name'],
-                'email': user['email'],
                 'id': user['id']
             })}
             return jsonify({
