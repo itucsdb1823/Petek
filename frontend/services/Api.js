be8aba6107b922db5166478e9bca56729a214413
Yavuz KOCA <ykoca0808@gmail.com>
2018-10-29 20:52:34 +0300

    Create middleware after and before sending a request to backend

--- frontend/services/Api.js
+++ frontend/services/Api.js
@@ -1,8 +1,25 @@
 import axios from 'axios'
+import { clientStore } from "../store"
 
 export default () => {
-    return axios.create({
-        baseURL: 'http://localhost:5000/api',
-        transformResponse: data => JSON.parse(data)
-    })
+  return axios.create({
+    baseURL: 'http://localhost:5000/api',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    transformRequest: [(data) => {
+      let str = [];
+      for(let p in data)
+        if (data.hasOwnProperty(p) && data[p])
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(data[p]));
+
+      clientStore.commit('setLoading', true)
+      clientStore.commit('clearError')
+
+      return str.join("&");
+    }],
+    transformResponse: [(data, headers) => {
+      clientStore.commit('setLoading', false)
+
+      return JSON.parse(data)
+    }],
+  })
 }
