a5990c11dd14d464d2d2cca3ff1ead5f26ddddc3
hoxhajjoana <j15031002k@gmail.com>
2018-11-11 16:12:25 +0300

    Update dbinit.py functions

--- server/dbinit.py
+++ server/dbinit.py
@@ -17,8 +17,12 @@ yes = {'yes', 'y', 'ye', '', 'Y', 'Yes'}
 no = {'no', 'n', 'N', 'No'}
 
 DROP_STATEMENTS = [
+    "DROP TABLE IF EXISTS notes",
+    "DROP TABLE IF EXISTS courses",
+    "DROP TABLE IF EXISTS terms",
+    "DROP TABLE IF EXISTS tokens",
     "DROP TABLE IF EXISTS users",
-    "DROP TABLE IF EXISTS tokens"
+
 ]
 
 INIT_STATEMENTS = [
@@ -39,36 +43,31 @@ INIT_STATEMENTS = [
         user_id INTEGER NOT NULL REFERENCES users(id),
         token VARCHAR(255) PRIMARY KEY UNIQUE,
         revoked BOOLEAN DEFAULT FALSE,
-        created_at timestamp NOT NULL
+        created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
     )""",
-    """CREATE TABLE IF NOT EXISTS term(
+    """CREATE TABLE IF NOT EXISTS terms(
+        id SERIAL PRIMARY KEY,
         season VARCHAR(6),
-        year VARCHAR(6),
-        PRIMARY KEY(season, year)
-    )""",
-    """CREATE TABLE IF NOT EXISTS course(
-            id VARCHAR(4),
-            code INTEGER,
-            english BOOLEAN,
-            name VARCHAR(255),
-            PRIMARY KEY(id, code, english)
-        )""",
-    """CREATE TABLE IF NOT EXISTS note(
-        id SERIAL PRIMARY KEY UNIQUE,
-        title VARCHAR(255),
-        content TEXT,
-        lecturer VARCHAR(255),
-        link VARCHAR(255),
-        slug VARCHAR(255) UNIQUE,
-        course_id VARCHAR(4),
-        course_code INTEGER,
-        english BOOLEAN,
         term_year VARCHAR(6),
-        term_season VARCHAR(6),
+        CONSTRAINT unique_term UNIQUE (season, term_year)
+    )""",
+    """CREATE TABLE IF NOT EXISTS courses(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(4) UNIQUE
+        )""",
+    """CREATE TABLE IF NOT EXISTS notes(
+        id SERIAL PRIMARY KEY UNIQUE,
+        title VARCHAR(255) NOT NULL,
+        content TEXT,
+        lecturer VARCHAR(255) DEFAULT 'Unknown',
+        link VARCHAR(255) NOT NULL,
+        slug VARCHAR(255) UNIQUE NOT NULL,
+        course_id INTEGER NOT NULL REFERENCES courses(id),
+        course_code INTEGER,
+        english BOOLEAN DEFAULT FALSE,
+        term_id INTEGER REFERENCES terms(id),
         user_id INTEGER NOT NULL REFERENCES users(id),
-        created_at TIMESTAMP NOT NULL,
-        FOREIGN KEY (course_id, course_code, english) REFERENCES course(id, code, english),
-        FOREIGN KEY (term_year,term_season) REFERENCES term(season, year)
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )""",
 ]
 
@@ -94,16 +93,50 @@ def update_all():
     initialize(INIT_STATEMENTS)
 
 
-def generate_random_data(number_of_elements):
+def generate_random_data(users_number = 5, notes_number = 5, courses_number = 5):
     fake = Faker('tr_TR')
 
+    terms_number = 9
     fake_hash = bcrypt.generate_password_hash('secret').decode('utf-8')
     cur = conn.cursor()
-    for i in range(0, number_of_elements):
+    for i in range(0, users_number):
         cur.execute("""INSERT INTO users(name, email, password, confirmation_code, slug, created_at)
                     VALUES(%s, %s, %s, %s, %s, %s) returning id""",
                     (str(fake.name()), str(fake.email()), fake_hash, str(fake.user_name()),
                      str(fake.slug()), str(fake.date_time_this_month())))
+
+    for i in range(0, courses_number):
+        cur.execute("""INSERT INTO courses(name) VALUES(%s) returning id""",
+                    (str(fake.lexify(text="???", letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ")),))
+
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('GÜZ'), str('16/17')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+               (str('BAHAR'), str('16/17')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('YAZ'), str('16/17')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('GÜZ'), str('17/18')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('BAHAR'), str('17/18')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('YAZ'), str('17/18')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('GÜZ'), str('18/19')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('BAHAR'), str('15/16')))
+    cur.execute("""INSERT INTO terms(season, term_year) VALUES(%s, %s) returning id""",
+                (str('YAZ'), str('15/16')))
+
+    for i in range(0, notes_number):
+        cur.execute("""INSERT INTO notes(title, link, slug, course_id, course_code, term_id, user_id, created_at)
+                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s) returning id""",
+                    (str(fake.text()), str(fake.text()), str(fake.slug()),
+                     int(fake.random_int(min=1, max=courses_number)), int(fake.random_int(min=100, max=600)),
+                     int(fake.random_int(min=1, max=terms_number)), int(fake.random_int(min=1, max=users_number)),
+                     str(fake.date_time_this_month())
+                     ))
+
     conn.commit()
     cur.close()
 
@@ -121,8 +154,12 @@ if __name__ == "__main__":
         if choice == 2:
             drop_all()
         if choice == 3:
-            print("Please enter the number of random elements: ")
-            number_of_elements = int(input())
-            generate_random_data(number_of_elements)
+            print("Please enter the number of random users: ")
+            users_number = int(input())
+            print("Please enter the number of random notes: ")
+            notes_number= int(input())
+            print("Please enter the number of random courses: ")
+            courses_number = int(input())
+            generate_random_data(users_number, notes_number, courses_number)
         if choice == 4:
             exit()
