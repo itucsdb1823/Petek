06fb6fbeb5a9afd44240e7ef857413fd52bf1d28
Yavuz KOCA <ykoca0808@gmail.com>
2018-11-27 01:54:47 +0300

    Add Note endpoints

--- server/routes.py
+++ server/routes.py
@@ -4,6 +4,9 @@ from random import *
 from flask_restful import Api
 from flask_cors import CORS
 from server.resources.auth import *
+from server.resources.notes import *
+from server.resources.courses import *
+from server.resources.terms import *
 
 cors = CORS(server, resources={r"/api/*": {"origins": "*"}})
 
@@ -12,6 +15,12 @@ api = Api(server)
 api.add_resource(Register, '/api/register')
 api.add_resource(Login, '/api/login')
 api.add_resource(Account, '/api/account')
+api.add_resource(Notes, '/api/notes')
+api.add_resource(NoteCreate, '/api/notes/store')
+api.add_resource(NoteSingle, '/api/notes/<string:note_slug>')
+api.add_resource(NoteDelete, '/api/notes/<int:note_id>/delete')
+api.add_resource(Courses, '/api/courses')
+api.add_resource(Terms, '/api/terms')
 
 
 @server.route('/', defaults={'path': ''})
