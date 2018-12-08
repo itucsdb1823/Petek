5fd905b721efbc1439e3025483d5cba954c09d9e
Yavuz KOCA <ykoca0808@gmail.com>
2018-12-08 18:58:53 +0300

    update lecturer and grade distribution models

--- server/models/GradeDistribution.py
+++ server/models/GradeDistribution.py
@@ -5,97 +5,69 @@ import random
 import datetime
 import time
 
+from server.models.Course import Course
+from server.models.Lecturer import Lecturer
+from server.models.Term import Term
+from server.models.User import User
+
 
 class GradeDistribution(Base):
-    id = 0
-    lecturer_id = 0
-    user_id = 0
-    image = ''
-    course_id = 0
-    course_code = 0
-    english = False
-    term_id = 0
-    created_at = ''
+    ATTRIBUTES = {}
+    COLUMNS = {}
+    HIDDEN = {}
+    TABLE = 'grade_distributions'
 
-    def __init__(self, lecturer_id=0, user_id=0, image='', course_id=0, course_code=0, english=False, term_id=0, _id=0):
-        self.errors = []
-        self.lecturer_id = lecturer_id
-        self.user_id = user_id
-        self.image = image
-        self.course_code = course_code
-        self.course_id = course_id
-        self.english = english
-        self.term_id = term_id
-        self.id = _id
-
-    def all(self):
-        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cur.execute("SELECT * FROM courses ORDER BY name ASC")
-        courses = cur.fetchall()
-        cur.close()
-        return courses
-
-    def save(self):
-        if self.validate() is False:
-            return False
-
-        ts = time.time()
-        self.created_at = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-        cur.execute(
-            "INSERT INTO grade_distributions"
-            "(lecturer_id, user_id, image, course_id, course_code, english, term_id, created_at) "
-            "VALUES(%s, %s, %s, %s, %s, %s, %s, %s) returning id",
-            (
-                int(self.lecturer_id), int(self.user_id), self.image, int(self.course_id),
-                int(self.course_code), bool(self.english), int(self.term_id), str(self.created_at)
-            ))
-
-        self.id = cur.fetchone()['id']
-        conn.commit()
-        cur.close()
-        return True
+    def __init__(self):
+        super().__init__()
+        self.ATTRIBUTES = {
+            'id': '',
+            'lecturer_id': '',
+            'user_id': '',
+            'image': '',
+            'course_id': '',
+            'course_code': '',
+            'english': False,
+            'term_id': ''
+        }
+        self.COLUMNS = {
+            'lecturer_id': '',
+            'user_id': '',
+            'image': '',
+            'course_id': '',
+            'course_code': '',
+            'english': '',
+            'term_id': '',
+        }
+        self.HIDDEN = {}
 
     def validate(self):
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM terms WHERE id=%s LIMIT 1", (self.term_id,))
-        term = cur.fetchone()
-        cur.close()
-        if term is None:
-            self.errors.append("Term could not found!")
+        term = Term().where('id', self.ATTRIBUTES['term_id']).first()
+        if term.exists() is False:
+            self.setError('Term could not found')
+        else:
+            self.plus('term', term.data())
 
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM lecturers WHERE id=%s LIMIT 1", (self.lecturer_id,))
-        lecturer = cur.fetchone()
-        cur.close()
-        if lecturer is None:
-            self.errors.append("Lecturer could not found!")
+        lecturer = Lecturer().where('id', self.ATTRIBUTES['lecturer_id']).first()
+        if lecturer.exists() is False:
+            self.setError('Lecturer could not found')
+        else:
+            self.plus('lecturer', lecturer.data())
 
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM courses WHERE id=%s LIMIT 1", (self.course_id,))
-        course = cur.fetchone()
-        cur.close()
-        if course is None:
-            self.errors.append("Course could not found!")
+        course = Course().where('id', self.ATTRIBUTES['course_id']).first()
+        if course.exists() is False:
+            self.setError('Course could not found')
+        else:
+            self.plus('course', course)
 
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM users WHERE id=%s LIMIT 1", (self.user_id,))
-        user = cur.fetchone()
-        cur.close()
-        if user is None:
-            self.errors.append("User could not found!")
+        user = User().where('id', self.ATTRIBUTES['user_id']).first()
+        if user.exists() is False:
+            self.setError('User could not found')
+        else:
+            self.plus('user', user)
 
-        if self.errors:
-            return False
-
-        return True
+        if not self.getErrors():
+            return True
+        return False
 
     def generateImageName(self):
         return ''.join(random.choice('abcde') for _ in range(50)) + '.jpg'
-
-    def delete(self):
-        cur = conn.cursor()
-        cur.execute("DELETE FROM grade_distributions WHERE id=%s AND user_id=%s", (self.id, self.user_id))
-        conn.commit()
-        cur.close()
